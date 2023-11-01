f11c91e89a06a8394f1f0ce763f3d92d821d94ce
ArthurGas <21a.gastiev07@gmail.com>
2023-11-01 08:08:34 +0300

    .

--- interface.h
+++ interface.h
@@ -9,17 +9,19 @@ class Interface
 private:
     std::string data;
     std::string b_data;
+    int work_sock;
 public:
     Interface(std::string d=" ")
     {
-        data = b_data = d;
+        data = d;
     }
     //Interface() = delete;
     //friend std::istream& operator >> (std::istream& cin, Interface& obj);
-    std::string data_read(int argc, char *argv[]);
+    std::string data_set(std::string str);
     std::string data_get() const;
     std::string data_byte(std::string str);
     std::string data_unbyte(std::string str);
+    void operator()(int sock);
    // void write_help();
 
 };
