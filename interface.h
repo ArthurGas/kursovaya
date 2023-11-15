9c642e4a9163ce89759c05710f4a9dcbc1498dcd
ArthurGas <21a.gastiev07@gmail.com>
2023-11-15 08:18:36 +0300

    header-файл интерфейса

--- interface.h
+++ interface.h
@@ -1,27 +1,42 @@
 #pragma once
 #include <string>
 #include <iostream>
+#include <fstream> 
+// C++
+#include <iostream>
+#include <memory>
+#include <random>
+#include <map>
 #include <fstream>
-#include <boost/program_options.hpp>
+
+// POSIX
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+
+//Crypto++
+#include <cryptopp/cryptlib.h>
+#include <cryptopp/hex.h>
+#include <cryptopp/files.h>
+#define CRYPTOPP_ENABLE_NAMESPACE_WEAK 1
+#include <cryptopp/md5.h>
 
 class Interface
 {
 private:
+    int work_sock;
     std::string data;
     std::string b_data;
-    int work_sock;
 public:
-    Interface(std::string d=" ")
-    {
-        data = d;
-    }
+    Interface(): work_sock(-1) 
+    {}
     //Interface() = delete;
     //friend std::istream& operator >> (std::istream& cin, Interface& obj);
-    std::string data_set(std::string str);
-    std::string data_get() const;
-    std::string data_byte(std::string str);
-    std::string data_unbyte(std::string str);
     void operator()(int sock);
-   // void write_help();
+    std::string data_set(std::string str);
+    //std::string string_recv();
+    //std::string data_byte(std::string str);
+    // std::string data_unbyte(std::string str);
+    // void write_help();
 
 };
