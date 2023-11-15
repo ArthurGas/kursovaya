e78ef207fb98749f47beb94cc0218e3b50846608
ArthurGas <21a.gastiev07@gmail.com>
2023-11-15 09:01:07 +0300

    -

---
+++ auth.h
@@ -0,0 +1,38 @@
+#pragma once
+// C++
+#include <iostream>
+#include <memory>
+#include <random>
+#include <map>
+#include <fstream>
+
+// POSIX
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+
+//Crypto++
+#include <cryptopp/cryptlib.h>
+#include <cryptopp/hex.h>
+#include <cryptopp/files.h>
+#define CRYPTOPP_ENABLE_NAMESPACE_WEAK 1
+#include <cryptopp/md5.h>
+
+#include "interface.h"
+class Auth
+{
+private:
+    int SALT;
+    int work_sock;
+    std::map<std::string, std::string> base;
+    // User user;
+public:
+    Auth(): work_sock(-1) {}
+    void operator()(int sock);
+    void auth();
+    //ident();
+    std::string string_recv();
+    void base_read(std::string file_name);
+    //hash();
+
+};
