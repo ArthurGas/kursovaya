37eac172e2220409599c87d6018966adfe5ced5b
ArthurGas <21a.gastiev07@gmail.com>
2023-11-15 08:15:51 +0300

    head-файл калькулятора

--- connection.h
+++ connection.h
@@ -13,21 +13,23 @@
 #include <netinet/in.h>
 #include <errno.h>
 #include <memory.h>
+#include "calc.h"
+#include "auth.h"
 #include "interface.h"
-
+#include <ctime>
 //const int def_qlen = 5;
 //const int def_port = 33333;
 
 class Connection
 {
 private:
+    
     int sock;
     std::unique_ptr <sockaddr_in> serv_addr;
     std::unique_ptr <sockaddr_in> client_addr;
     int queue_len;
-
 public:
-    void connect(Interface & str);
+    void connect(Calculation& clc, Auth& ath, Interface& intr);
     void disconnect();
     void wait();
     Connection(unsigned short port, int qlen);
