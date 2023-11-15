3f41da10b985471f79ddffd1b5c7beef851c90a9
ArthurGas <21a.gastiev07@gmail.com>
2023-11-15 08:13:29 +0300

    header-файл калькулятора

--- calc.h
+++ calc.h
@@ -2,22 +2,41 @@
 #include <stdint.h>
 #include <vector>
 #include <iostream>
-class Calculation
+#include <errno.h>
+#include <float.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <cmath>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <typeinfo>
+#include <memory>
+#include <system_error>
+#include <typeinfo>
+#include <limits>
+#include "interface.h"
+
+class Calculation: private Interface
 {
 private:
-    float result=0;
+    float result=1.0;
     uint32_t count;
     std::vector<float> vec_mean;
     uint32_t size;
+    int work_sock;
 
 public:
-    float multip();
-    bool overflow();
+    Calculation(): work_sock(-1)
+    {}
+    void multip();
+    void operator()(int sock);
+    float overflow(float res);
 };
 
 //class Vector
 //{
 //private:
- //   std::vector<float> vec_mean;
- //   uint32_t size;
+//   std::vector<float> vec_mean;
+//   uint32_t size;
 //};
