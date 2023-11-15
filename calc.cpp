0128aee820d30db35bd0faa61b332dfa34c65514
ArthurGas <21a.gastiev07@gmail.com>
2023-11-15 08:13:09 +0300

    Cpp-файл калькулятора

--- calc.cpp
+++ calc.cpp
@@ -1,57 +1,51 @@
 #include "calc.h"
-
 void Calculation::operator()(int sock)
 {
     work_sock = sock;
     Calculation::multip();
 }
-//float
+
+float Calculation::overflow(float res)
+{
+    if(res==INFINITY)
+        return(FLT_MAX);
+    else if(res==-INFINITY)
+        return(FLT_MAX);
+    else
+        return res;
+}
+
 void Calculation::multip()
 {
     int rc;
-    size_t len = 32; //начальный размер массива принимаемых данных
-    char buf[len];
-    char msg[sizeof(uint32_t)];
+    //size_t len = 32; //начальный размер массива принимаемых данных
+    // char buf[len];
+    float value;
+    //char msg[sizeof(uint32_t)];
     std::vector<float> msg_cont;
-    float mult1;
-    float mult2;
-    rc = recv(work_sock, buf, len, 0);
+    rc = recv(work_sock, &count, sizeof(uint32_t), 0);
     if (rc == -1)
-        throw std::system_error(errno, std::generic_category(), "Recv number of vectors error");
-    for(uint i=0; i<sizeof(uint32_t); i++) {
-        msg[i]=buf[i];
-    }
-    count = atoi(msg);
+        throw std::system_error(errno, std::generic_category(), "Vector count recv error");
     std::clog << "log: Число векторов " << count << std::endl;
     for (uint32_t i=0; i < count; i++) {
-        rc = recv(work_sock, buf, len, 0);
+        rc = recv(work_sock, &size, sizeof(uint32_t), 0);
         if (rc == -1)
-            throw std::system_error(errno, std::generic_category(), "Recv vector size error");
-        for(uint i=0; i<sizeof(uint32_t); i++) {
-            msg[i]=buf[i];
-        }
-        size = atoi(msg);
+            throw std::system_error(errno, std::generic_category(), "Vector size recv error");
         std::clog << "log: Размер вектора " << size << std::endl;
         //std::clog << "log: Рачет значений вектора " << std::endl;
         std::clog << "log: Вектор [ ";
         for (uint32_t j=0; j < size; j++) {
-            rc = recv(work_sock, buf, len, 0);
+            rc = recv(work_sock, &value,sizeof(uint32_t), 0);
             if (rc == -1)
                 throw std::system_error(errno, std::generic_category(), "Recv vector error");
-            for(uint i=0; i<sizeof(uint32_t); i++) {
-                msg[i]=buf[i];
-            }
-            std::clog << atof(msg) << " ";
-            if(i%2!=0){
-                mult1 = atof(msg);
-                result=result*mult1;
-            }else{
-                mult2 = atof(msg);
-                result=result*mult2;
-            }
+            std::clog << value << " ";
+            result*=value;
         }
+        float endres = overflow(result);
         std::clog << "]" << std::endl;
-        std::clog << "log: Произведение вектора: " << result << std::endl;
+        std::clog << "log: Произведение вектора: " << endres << std::endl;
+        send(work_sock, &endres, sizeof(uint32_t), 0);
+        result = 1;
     }
 }
 /*
